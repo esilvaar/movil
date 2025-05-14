91851e1ff1f90f238788a70f39274a9438d007cb
edupanpan <eduardo20032110@gmail.com>
2025-05-13 23:44:00 -0400

    probando

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                    Inicio()
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Inicio() {
+    Surface(modifier = Modifier.fillMaxSize(), color=Color.DarkGray) {
+        Column {
+            Text(
+                text = "Hello "
+
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Navbar()
+
+        }
+
+    }
+}
+@Composable
+fun Navbar(){
+    Text(
+        text="jhapldasjd"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Inicio()
+
+    }
+}
