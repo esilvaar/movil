a508248170261ce58eea72783e77ad4e8ad0f82a
edupanpan <eduardo20032110@gmail.com>
2025-05-14 01:07:57 -0400

    posicionamiento de navbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.composable
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,40 +35,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                    Inicio()
-                }
+                Inicio()
             }
         }
     }
+}
+
 
 
 @Composable
 fun Inicio() {
-    Surface(modifier = Modifier.fillMaxSize(), color=Color.DarkGray) {
-        Column {
-            Text(
-                text = "Hello "
+    Surface(
+        modifier = Modifier.fillMaxSize(),
 
+    ) {
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            Text(
+                text = "Hello ",
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(vertical = 16.dp)
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Navbar()
+
+
+            Navbar(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(vertical = 16.dp)
+            )
+
 
         }
-
     }
 }
+
 @Composable
-fun Navbar(){
-    Text(
-        text="jhapldasjd"
-    )
+fun Navbar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(Color.Red, shape = RoundedCornerShape(50.dp))
+            .padding(horizontal = 30.dp)
+            .height(50.dp),
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Button(onClick = { /* Acción al hacer clic */ }) {
+            Text("Btn 1")
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = { /* Acción al hacer clic */ }) {
+            Text("Btn 2")
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Button(onClick = { /* Acción al hacer clic */ }) {
+            Text("Btn 3")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
         Inicio()
-
     }
 }
